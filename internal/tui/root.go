d497da25e5f2e548240b05a72937f6b4281b52e7
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2026-02-17 10:00:36 +0800

    wip

--- internal/tui/root.go
+++ internal/tui/root.go
@@ -55,7 +55,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "q":
+		case "q", "ctrl+c":
 			return m, tea.Quit
 		}
 	case tea.WindowSizeMsg:
