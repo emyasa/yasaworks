578e7990a9331e5d612e7714c6fff6f544ab10cd
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2026-03-01 00:11:15 +0800

    identity tagging: saved in context

--- cmd/ssh/main.go
+++ cmd/ssh/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"log"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/ssh"
 	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/activeterm"
 	"github.com/charmbracelet/wish/bubbletea"
 	"github.com/emyasa/yasaworks/internal/tui"
+	"github.com/google/uuid"
+	gossh "golang.org/x/crypto/ssh"
 )
 
 func main() {
@@ -23,6 +28,21 @@ func main() {
 				
 				return model, []tea.ProgramOption{tea.WithAltScreen()}
 			}),
+			activeterm.Middleware(),
+		),
+		wish.WithPublicKeyAuth(func(ctx ssh.Context, key ssh.PublicKey) bool {
+			hash := md5.Sum(key.Marshal())
+			fingerprint := hex.EncodeToString(hash[:])
+			ctx.SetValue("fingerprint", fingerprint)
+			ctx.SetValue("anonymous", false)
+			return true
+		}),
+		wish.WithKeyboardInteractiveAuth(
+			func(ctx ssh.Context, challenger gossh.KeyboardInteractiveChallenge) bool {
+				ctx.SetValue("fingerprint", uuid.NewString())
+				ctx.SetValue("anonymous", true)
+				return true
+			},
 		),
 	)
 
