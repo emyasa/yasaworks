a4c9bd9b8235bd58f26dbd1438a0fc9d8a32b722
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2026-03-07 00:00:35 +0800

    initial migration: wip

---
+++ migrations/001_users.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    fingerprint TEXT NOT NULL UNIQUE,
+    created_at TEXT NOT NULL DEFAULT (datetime('now'))
+);
+
+CREATE TABLE user_ips (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    ip_address TEXT NOT NULL,
+    added_at TEXT NOT NULL DEFAULT (datetime('now')),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
