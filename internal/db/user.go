89f6c45debea81b705819d0d8bfc715621060a1e
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2026-03-07 21:09:23 +0800

    sets a timeout context

--- internal/db/user.go
+++ internal/db/user.go
@@ -1,10 +1,17 @@
 package db
 
+import (
+	"context"
+	"time"
+)
+
 type UpsertUserRequest struct {
 	fingerprint string
 	clientIP string
 }
 
-func (db *DB) UpsertUser(r UpsertUserRequest) {
+func (db *DB) UpsertUser(ctx context.Context, r UpsertUserRequest) {
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
 }
 
