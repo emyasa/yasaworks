41d844edbab4e8216f8aa85f8162e9e717a42884
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2026-05-01 10:01:37 +0800

    footer: nav guide

--- internal/tui/footer.go
+++ internal/tui/footer.go
@@ -1,25 +1,33 @@
 package tui
 
 import (
+	"strings"
+
 	"github.com/charmbracelet/lipgloss"
 )
 
 func (m model) footerView() string {
+	bold := m.theme.TextAccent().Bold(true).Render
+	base := m.theme.Base().Render
 	table := lipgloss.NewStyle().
 		Width(78).
 		BorderTop(true).
 		BorderStyle(lipgloss.NormalBorder()).
 		BorderForeground(m.theme.Border()).
 		PaddingBottom(1).
-		Align(lipgloss.Center).
-		Render()
+		Align(lipgloss.Right)
+
+	navCommands := []string{}
+	navCommands = append(navCommands, bold("j") + " " + base("↓"))
+	navCommands = append(navCommands, bold("k") + " " + base("↑") + " ")
+	footer := table.Render(strings.Join(navCommands, " "))
 
 	return lipgloss.Place(
 		m.widthContainer,
-		lipgloss.Height(table),
+		lipgloss.Height(footer),
 		lipgloss.Center,
 		lipgloss.Center,
-		table,
+		footer,
 	)
 }
 
